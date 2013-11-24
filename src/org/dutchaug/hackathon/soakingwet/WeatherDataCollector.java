1e85d9405d46de358d1f010f8e6f0d4ffc201999
arno koehler <arno.koehler@gmail.com>
2013-11-24 11:11:32 +0100

    data collector created

---
+++ src/org/dutchaug/hackathon/soakingwet/WeatherDataCollector.java
@@ -0,0 +1,47 @@
+package org.dutchaug.hackathon.soakingwet;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.squareup.okhttp.OkHttpClient;
+
+public class WeatherDataCollector {
+
+	private OkHttpClient client = new OkHttpClient();
+
+	public int getPrecipitationForLatLong(double latitude, double longtitude) throws IOException {
+		// between 0 and 255
+
+		String pipeSeperatedFile = get(new URL("http://gps.buienradar.nl/getrr.php?lat=52&lon=4"));
+
+		System.out.println(pipeSeperatedFile);
+		return 0;
+	}
+
+	private String get(URL url) throws IOException {
+		HttpURLConnection connection = client.open(url);
+		InputStream in = null;
+		try {
+			// Read the response.
+			in = connection.getInputStream();
+			byte[] response = readFully(in);
+			return new String(response, "UTF-8");
+		} finally {
+			if (in != null)
+				in.close();
+		}
+	}
+
+	private byte[] readFully(InputStream in) throws IOException {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		byte[] buffer = new byte[1024];
+		for (int count; (count = in.read(buffer)) != -1;) {
+			out.write(buffer, 0, count);
+		}
+		return out.toByteArray();
+	}
+
+}
