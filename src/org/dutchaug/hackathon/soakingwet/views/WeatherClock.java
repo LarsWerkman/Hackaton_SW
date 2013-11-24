ba55ad08a7d53e5c36702d44c077bb7f7838caa7
Lars Werkman <werkman.lars@gmail.com>
2013-11-24 11:29:59 +0100

    Basedownload async

--- src/org/dutchaug/hackathon/soakingwet/views/WeatherClock.java
+++ src/org/dutchaug/hackathon/soakingwet/views/WeatherClock.java
@@ -1,10 +1,18 @@
 package org.dutchaug.hackathon.soakingwet.views;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.View.MeasureSpec;
 
 public class WeatherClock extends View {
+	
+	/**
+	 * The radius of the color wheel.
+	 */
+	private int mColorWheelRadius;
+	private int mPreferredColorWheelRadius;
 
 	public WeatherClock(Context context) {
 		super(context);
@@ -23,6 +31,55 @@ public class WeatherClock extends View {
 	
 	public void init(AttributeSet attrs, int defStyle){
 		
+	}
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		super.onDraw(canvas);
+	}
+	
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		/*final int intrinsicSize = 2 * (mPreferredColorWheelRadius + mColorPointerHaloRadius);
+
+		int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+		int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+		int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+		int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+		int width;
+		int height;
+
+		if (widthMode == MeasureSpec.EXACTLY) {
+			width = widthSize;
+		} else if (widthMode == MeasureSpec.AT_MOST) {
+			width = Math.min(intrinsicSize, widthSize);
+		} else {
+			width = intrinsicSize;
+		}
+
+		if (heightMode == MeasureSpec.EXACTLY) {
+			height = heightSize;
+		} else if (heightMode == MeasureSpec.AT_MOST) {
+			height = Math.min(intrinsicSize, heightSize);
+		} else {
+			height = intrinsicSize;
+		}
+
+		int min = Math.min(width, height);
+		setMeasuredDimension(min, min);
+		mTranslationOffset = min * 0.5f;
+
+		// fill the rectangle instances.
+		mColorWheelRadius = min / 2 - mColorWheelThickness - mColorPointerHaloRadius;
+		mColorWheelRectangle.set(-mColorWheelRadius, -mColorWheelRadius,
+				mColorWheelRadius, mColorWheelRadius);
+
+		mColorCenterRadius = (int) ((float) mPreferredColorCenterRadius * ((float) mColorWheelRadius / (float) mPreferredColorWheelRadius));
+		mColorCenterHaloRadius = (int) ((float) mPreferredColorCenterHaloRadius * ((float) mColorWheelRadius / (float) mPreferredColorWheelRadius));
+		mCenterRectangle.set(-mColorCenterRadius, -mColorCenterRadius,
+				mColorCenterRadius, mColorCenterRadius);*/
 	}
 
 }
