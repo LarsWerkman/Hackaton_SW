77c8e8972601cd333ca042ae92e8e19d45c4bc37
Rhijn, M van - SPLXL <martin-van.rhijn@klm.com>
2013-11-24 11:40:10 +0100

    show last know location

--- src/org/dutchaug/hackathon/soakingwet/MainActivity.java
+++ src/org/dutchaug/hackathon/soakingwet/MainActivity.java
@@ -1,17 +1,32 @@
 package org.dutchaug.hackathon.soakingwet;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import org.dutchaug.hackathon.soakingwet.dialog.ErrorDialogFragment;
 
-public class MainActivity extends Activity {
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+
+public class MainActivity extends FragmentActivity implements GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener {
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private LocationClient locationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        locationClient = new LocationClient(this, this, this);
         setContentView(R.layout.activity_main);
-    }
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,5 +34,65 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        locationClient.connect();
+    }
+
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
+        locationClient.disconnect();
+        super.onStop();
+    }
+
+    /*
+     * Handle results returned to the FragmentActivity by Google Play services
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Decide what to do based on the original request code
+        switch (requestCode) {
+            case CONNECTION_FAILURE_RESOLUTION_REQUEST:
+                /*
+                 * If the result code is Activity.RESULT_OK, try to connect again
+                 */
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        /*
+                         * Try the request again
+                         */
+                        break;
+                }
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult arg0) {
+        // Get the error code
+        // Get the error dialog from Google Play services
+        Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(arg0.getErrorCode(), this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+        // If Google Play services can provide an error dialog
+        if (errorDialog != null) {
+            // Create a new DialogFragment for the error dialog
+            ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+            // Set the dialog in the DialogFragment
+            errorFragment.setDialog(errorDialog);
+            // Show the error dialog in the DialogFragment
+            errorFragment.show(getFragmentManager(), "Location Updates");
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle arg0) {
+        Location lastLocation = locationClient.getLastLocation();
+        TextView textView = (TextView) findViewById(R.id.mainactivity_text);
+        textView.setText("lat: " + lastLocation.getLatitude() + ", long: " + lastLocation.getLongitude());
+    }
+
+    @Override
+    public void onDisconnected() {
+        // TODO Auto-generated method stub
+    }
 }
--- src/org/dutchaug/hackathon/soakingwet/SoakingWetApplication.java
+++ src/org/dutchaug/hackathon/soakingwet/SoakingWetApplication.java
@@ -2,6 +2,16 @@ package org.dutchaug.hackathon.soakingwet;
 
 import android.app.Application;
 
+/**
+ * The Class SoakingWetApplication.
+ */
 public class SoakingWetApplication extends Application {
 
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
 }
